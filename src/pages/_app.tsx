394df94c2dd4f9baa879bbd5a23a9229e3160672
Raystar9 <raystarkoo@gmail.com>
2023-10-03 19:47:57 +0900

    cors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from "next/app";
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 export default function App({Component, pageProps}: AppProps) {
   return <div style={{maxWidth:"1100px", margin:"auto"}}><Component {...pageProps}></Component></div>
