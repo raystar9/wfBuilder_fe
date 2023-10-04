147207e673ba056ca73015b1e86aeae9014af535
Raystar9 <raystarkoo@gmail.com>
2023-10-05 00:25:53 +0900

    a

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
         includePaths: [path.join(__dirname, 'styles')],
         prependData: `@import "@/styles/variables.scss";`,
     },
+    images: {
+        minimumCacheTTL: 2592000,
+    },
 }
 
 module.exports = nextConfig
